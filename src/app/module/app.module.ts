6226220fe0a7bcb98ee7840e55e68919f58649c8
farhang <farhang.ezziirani@gmail.com>
2023-05-13 20:24:48 +0330

    farhang irani 1401/02/23 part 2

--- src/app/module/app.module.ts
+++ src/app/module/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import {HeaderInterceptor} from "../network/header/header.interceptor";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import {HeaderInterceptor} from "../network/header/header.interceptor";
   imports: [
     BrowserModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [
     {
